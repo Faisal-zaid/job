59ac9ddb4a640043e0bdaad5ebe988eb8949c4ac
Brizzy <Brizzyy98@gmail.com>
2026-01-14 14:49:08 +0300

    adds App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import Home from "./pages/Home";
-import EmployerDashboard from "./pages/EmployerDashboard";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Navbar from "./components/Navbar"
+import Home from "./pages/Home"
+import Jobs from "./pages/Jobs"
+import JobDetails from "./pages/JobDetails"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import Dashboard from "./pages/Dashboard"
 
 function App() {
   return (
-   <>
-     <Home/>
-   </>
-   );
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/jobs" element={<Jobs />} />
+        <Route path="/jobs/:id" element={<JobDetails />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
