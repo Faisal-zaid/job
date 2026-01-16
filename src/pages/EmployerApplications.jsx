a2592bfddca3dbc62a059f9fab7d3e66716048c0
Faisal Zaid <zaidalideveloper@gmail.com>
2026-01-16 10:03:33 +0300

    added fetching of applications

--- src/pages/EmployerApplications.jsx
+++ src/pages/EmployerApplications.jsx
@@ -1,1 +1,18 @@
 import React, { useState, useEffect } from "react";
+
+const EmployerApplications = () => {
+  const [applications, setApplications] = useState([]);
+  const token = localStorage.getItem("token");
+
+  const fetchApplications = async () => {
+  try {
+    const res = await fetch("http://127.0.0.1:5000/employer/applications", {
+      headers: { Authorization: `Bearer ${token}` }
+    });
+    const data = await res.json();
+    setApplications(data.applications || []);
+  } catch (err) {
+    console.error("Error fetching applications:", err);
+    setApplications([]);
+  }
+};
