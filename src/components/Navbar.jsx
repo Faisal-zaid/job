08fd0828bca7bbaef231e8445486bc388fdc21ea
Ephrahim Anyanje <atwoli2345@gmail.com>
2026-01-17 16:37:49 +0300

    made major adjustments to navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,16 @@
 
-import { Link } from "react-router-dom";
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-function Navbar() {
-  return (
-    <nav>
-      <Link to="/">Home</Link> | <Link to="/jobs">Jobs</Link> |{" "}
-      <Link to="/login">Login</Link> | <Link to="/dashboard">Dashboard</Link>
-    </nav>
-  );
-}
+const Navbar = ({ user, setUser }) => {
+  const navigate = useNavigate();
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("role");
+    setUser(null);
+    navigate("/login");
+  };
 
 export default Navbar;
 import React from 'react'
