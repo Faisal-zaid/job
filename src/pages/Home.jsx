9675734abc0ac0a0933d00a7bc5418a8a581c6b9
Enock Cheruiyot <cheruiyote518@gmail.com>
2026-01-18 11:29:54 +0300

    styles jobseeker

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,10 @@ import About from "../components/About";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
-
 function Home() {
   return (
-    <div>
-     
+    // Ensure the home content doesn't fight the Navbar for "stacking" space
+    <div className="relative z-0">
       <Header />
       <About />
       <Footer />
